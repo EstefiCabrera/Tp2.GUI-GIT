fd06206724e3d3ecd1ad9f1ef3327e5741ec9390
Estefania Cabrera <estefi.cabrera22@gmail.com>
2026-08-27 16:08:03 -0300

    Initial commit

---
+++ src/tp2/gui/git/Tp2GUIGIT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2.gui.git;
+
+/**
+ *
+ * @author estef
+ */
+public class Tp2GUIGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
